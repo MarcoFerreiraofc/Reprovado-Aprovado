0af052481301e4d87925d0c314017bdc97202b08
Marco Ferreira <marcoantonio.fa@outlook.com>
2020-11-13 10:14:32 -0300

    projeto aprovado reprovado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            string situacao = "";
+
+            Console.WriteLine("Digite sua primeira nota"); 
+            double nota1 = double.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("Digite sua segunda nota"); 
+            double nota2 = double.Parse(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+            if(media > 7){ 
+                situacao = "Aprovado"; 
+            }
+            else{ 
+                situacao = "Reprovado";
+            }
+
+            Console.WriteLine(situacao);
+        }
+    }
+}
